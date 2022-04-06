c3398565ea9cac698929455578cba3170bbe053e
Joe Maples <joe@maples.dev>
2022-04-06 10:35:58 -0400

    wifi: Don't include prebuilt kernel modules
    Change-Id: I58ef50c49ea95acd947519df670931a67de0c0a6

--- wifi/wifi.mk
+++ wifi/wifi.mk
@@ -1,8 +1,3 @@
-WIFI_KO_FILES := $(shell find $(TOPDIR)kernel/drivers/net/wireless/rockchip_wlan -name "*.ko" -type f)
-
-BOARD_VENDOR_KERNEL_MODULES += \
-	$(foreach file, $(WIFI_KO_FILES), $(file))
-
 PRODUCT_COPY_FILES += \
         $(CUR_PATH)/wifi/iwconfig:$(TARGET_COPY_OUT_VENDOR)/bin/iwconfig \
         $(CUR_PATH)/wifi/iwlist:$(TARGET_COPY_OUT_VENDOR)/bin/iwlist
